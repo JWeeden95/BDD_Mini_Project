2f7b0853b1a807a109127e5fc91b372b7006e2a1
sidkhosla <sidkhosla2@gmail.com>
2021-02-04 21:51:49 +0530

    Summary Page ALL DONEEE for MVP

--- src/test/java/com/sparta/jw/cucumber/stepdefs/SummaryPageStepdef.java
+++ src/test/java/com/sparta/jw/cucumber/stepdefs/SummaryPageStepdef.java
@@ -1,31 +1,45 @@
 package com.sparta.jw.cucumber.stepdefs;
 
 
+import com.sparta.jw.pom.pages.*;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SummaryPageStepdef {
-    String SummaryPageURL;
     boolean isTheCartAmountCorrect;
+    WebDriver webDriver = new ChromeDriver();
+    private HomePage homePage = new HomePage(webDriver);
+    private SummaryPage summaryPage;
+    private AddressPage addressPage;
 
     @Given("I am on the order summary page")
     public void iAmOnTheOrderSummaryPage() {
+        homePage.goToHomePage();
+        SignInPage signInPage = homePage.goToSignInPageFromHomePage();
+        MyAccountPage myAccountPage = signInPage.goToMyAccountPageFromSignInPage();
+        homePage = myAccountPage.goToHomePageFromMyAccountPage();
+        homePage.addFirstItemToBasket();
+        summaryPage = homePage.goToSummaryPageFromHomePage();
     }
 
     @And("I have {int} item")
     public void iHaveSomethingInMyCart(int arg0) {
+        isTheCartAmountCorrect = summaryPage.correctCartAmountOnTheSummaryPage(arg0);
     }
 
     @When("I click proceed to checkout from Summary page")
     public void iClickProceedToCheckoutFromSummaryPage() {
+        addressPage = summaryPage.goToAddressPageFromSummaryPage();
     }
 
     @Then("Address page appears")
     public void addressPageAppears() {
-        Assertions.assertEquals("http://automationpractice.com/index.php?controller=order", SummaryPageURL);
-        Assertions.assertTrue(isTheCartAmountCorrect);
+        Assertions.assertEquals("http://automationpractice.com/index.php?controller=order&step=1", addressPage.getUrl());
+        webDriver.close();
     }
 }
