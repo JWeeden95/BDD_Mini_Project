1704198400c8c798b69bdf4d76826e3d2271ac9d
Ben Middlehurst <bmiddlehurst@spartaglobal.com>
2021-02-04 10:19:24 +0000

    updates to PaymentMethodPage

--- src/test/java/com/sparta/jw/cucumber/stepdefs/PaymentMethodPageStepDefs.java
+++ src/test/java/com/sparta/jw/cucumber/stepdefs/PaymentMethodPageStepDefs.java
@@ -1,23 +1,30 @@
 package com.sparta.jw.cucumber.stepdefs;
 
-import com.sparta.jw.pom.pages.PaymentMethodPage;
+import com.sparta.jw.pom.pages.*;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PaymentMethodPageStepDefs {
-    private WebDriver webDriver;
+    WebDriver webDriver = new ChromeDriver();
+    HomePage homePage;
+    SummaryPage summaryPage;
+    SignInPage signInPage;
+    AddressPage addressPage;
 
     @Given("I am on the Payment Method Page")
     public void iAmOnThePaymentMethodPage() {
+        homePage.goToHomePage();
+        homePage.goToSignInPage();
 
     }
 
     @When("I click {string}")
     public void iClickPayByBankWire() {
-        PaymentMethodPage.payByBankWire();
+        PaymentMethodPage.goToPayByBankWire();
     }
 
     @Then("The bank wire payment page appears")
