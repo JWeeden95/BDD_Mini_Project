bcc75d36e6e243db843ad2daaaa92a2390062dd2
sidkhosla <sidkhosla2@gmail.com>
2021-02-04 17:05:45 +0530

    Summary Page- New cart counter and tests for it+ Some refactors

--- src/test/java/com/sparta/jw/pom/pages/SummaryPage.java
+++ src/test/java/com/sparta/jw/pom/pages/SummaryPage.java
@@ -8,19 +8,24 @@ import org.openqa.selenium.WebDriver;
 public class SummaryPage {
     //http://automationpractice.com/index.php?controller=order
     private static WebDriver webDriver;
-
+    private static String summaryPageLink = "http://automationpractice.com/index.php?controller=order";
     public SummaryPage(WebDriver driver) {
         webDriver = driver;
     }
 
 
-    public void goToSummaryPage(String link) {
-        webDriver.get(link);
+    public void goToSummaryPage() {
+        webDriver.get(summaryPageLink);
     }
 
     public AddressPage goToAddressPage() {
         webDriver.findElement(By.linkText("Proceed to checkout")).click();
         return new AddressPage(webDriver);
+    }
+    public static boolean correctCartAmountOnTheSummaryPage(int itemCount) {
+        String total = webDriver.findElement(By.id("summary_products_quantity")).getText();
+        String itemCountInString = itemCount + "";
+        return total.contains(itemCountInString);
     }
 
     //Added by me for testing on LetsDoThis class, remove if you need to (JF)
