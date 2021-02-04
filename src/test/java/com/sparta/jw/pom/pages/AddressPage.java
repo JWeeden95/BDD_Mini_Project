42517d9f81f4753785f8336a375ee35829bb9691
MrFletcha <jwefletcher@hotmail.co.uk>
2021-02-04 12:05:33 +0000

    Properties file added, with associated properties creation and fileWriter, its a little messy, could do with making a properties class maybe?

--- src/test/java/com/sparta/jw/pom/pages/AddressPage.java
+++ src/test/java/com/sparta/jw/pom/pages/AddressPage.java
@@ -4,19 +4,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 public class AddressPage extends Page{
 
     //http://automationpractice.com/index.php?controller=order&step=1
+
+    private static final Properties properties = new Properties();
+    private static final String PROPERTIES_PATH = "src/test/resources/userDetails.properties";
     List<String> bAddress = new ArrayList<>();
     List<String> sAddress = new ArrayList<>();
+    List<String> actualBillingAddress = new ArrayList<>();
     By comments = new By.ByClassName("form-control");
+
+    public List<String> getActualBillingAddress() {
+        return actualBillingAddress;
+    }
 
     public AddressPage(WebDriver webDriver)
     {
+        createProperties();
+        setActualBillingAddress();
         this.webDriver = webDriver;
+    }
+
+    private static void createProperties() {
+        try {
+            properties.load(new FileReader(PROPERTIES_PATH));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean isCommentEmpty()
@@ -44,9 +66,21 @@ public class AddressPage extends Page{
         return bAddress;
     }
 
+    public void setActualBillingAddress()
+    {
+        String address = properties.getProperty("address");
+        actualBillingAddress.addAll(Arrays.asList(address.split(",")));
+    }
+
     public boolean shippingIsBilling()
     {
         String string = webDriver.findElement(By.id("addressesAreEquals")).getText();
         return string.equals("checked");
+    }
+
+    public ShippingPage goToShipping()
+    {
+        webDriver.findElement(By.name("processAddress")).click();
+        return new ShippingPage(webDriver);
     }
 }
--- src/test/java/com/sparta/jw/pom/pages/PaymentMethodPage.java
+++ src/test/java/com/sparta/jw/pom/pages/PaymentMethodPage.java
@@ -34,3 +34,4 @@ public class PaymentMethodPage extends Page{
 }
 
 
+
