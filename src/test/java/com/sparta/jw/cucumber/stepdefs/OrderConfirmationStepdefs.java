0c08b1b5e20c9dec412e10ae129db4d73d9ee914
Asakar <asakarh@gmail.com>
2021-02-04 12:32:59 +0000

    issue number #19 OrderConfirmationStepdefs updated

--- src/test/java/com/sparta/jw/cucumber/stepdefs/OrderConfirmationStepdefs.java
+++ src/test/java/com/sparta/jw/cucumber/stepdefs/OrderConfirmationStepdefs.java
@@ -1,20 +1,37 @@
 package com.sparta.jw.cucumber.stepdefs;
 
-import com.sparta.jw.pom.pages.BankWirePaymentPage;
-import com.sparta.jw.pom.pages.HomePage;
+import com.sparta.jw.pom.pages.*;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OrderConfirmationStepdefs {
 
     private BankWirePaymentPage bankWirePaymentPage;
-    private WebDriver webDriver;
+    WebDriver webDriver = new ChromeDriver();
+    HomePage homePage = new HomePage(webDriver);
+    SignInPage signInPage;
+    MyAccountPage myAccountPage;
+    SummaryPage summaryPage;
+    AddressPage addressPage;
+    ShippingPage shippingPage;
+    PaymentMethodPage paymentMethodPage;
 
     @Given("that I am on the bank wire page")
     public void thatIAmOnTheBankWirePage() {
-        bankWirePaymentPage = new BankWirePaymentPage(webDriver);
+        homePage.goToHomePage();
+        signInPage = homePage.goToSignInPage();
+        myAccountPage = signInPage.goToMyAccountPageFromSignInPage();
+        homePage = myAccountPage.goToHomePageFromMyAccountPage();
+        summaryPage = homePage.goToSummaryPageFromHomePage();
+        addressPage = summaryPage.goToAddressPageFromSummaryPage();
+        shippingPage = addressPage.goToShippingPageFromAdressPage();
+        shippingPage.clickConfirmCheckbox();
+        paymentMethodPage = shippingPage.goToPaymentMethodPage();
+        bankWirePaymentPage = paymentMethodPage.goToPayByBankWire();
     }
 
     @When("I click confirm order")
